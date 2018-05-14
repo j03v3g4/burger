bb7d788438db9cb02b98785abf98dddb4e26732b
Joseph Vega <1josephvega@gmail.com>
2018-05-14 16:10:11 -0400

    Initial folder structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Cheeseburger');
